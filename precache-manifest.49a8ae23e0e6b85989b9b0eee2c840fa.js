93e7c5b22f2e6cde2cf548761e439f5d5c700114
Kai Brabo <kaibrabo@gmail.com>
2019-03-09 20:12:19 +0000

    Updates

---
+++ precache-manifest.49a8ae23e0e6b85989b9b0eee2c840fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a59fc43a53cbd43722a",
+    "url": "/reslist/static/css/main.8b8c4a58.chunk.css"
+  },
+  {
+    "revision": "1a59fc43a53cbd43722a",
+    "url": "/reslist/static/js/main.1a59fc43.chunk.js"
+  },
+  {
+    "revision": "192a331458ff0ced0e52",
+    "url": "/reslist/static/js/runtime~main.192a3314.js"
+  },
+  {
+    "revision": "f6f5cbc2281ad1a9efcb",
+    "url": "/reslist/static/js/2.f6f5cbc2.chunk.js"
+  },
+  {
+    "revision": "213ed360b0e60704e495d7bef1e32c01",
+    "url": "/reslist/index.html"
+  }
+];
